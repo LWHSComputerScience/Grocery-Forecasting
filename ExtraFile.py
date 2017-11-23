09ed105b24c396aec78ed897bb3ac52a0c32fd6c
nrocketmann <nameerhirschkind@gmail.com>
2017-11-22 20:20:12 -0800

    d

--- ExtraFile.py
+++ ExtraFile.py
@@ -1,20 +1,31 @@
-import csv
-import random
-with open('data/train.csv') as f:
-    reader = csv.reader(f, delimiter=',')
-    allrows = []
-    s = random.sample(population=[i for i in range(1,100000000)], k=10000)
-    print(s)
-    for n, row in enumerate(reader):
-        if n==0:
-            allrows.append(row)
-        if n in s:
-            allrows.append(row)
-        if n>100000000:
-            break
-print(allrows)
-with open('data/smalltrain.csv','w') as fw:
-    writer = csv.writer(fw, delimiter=',')
-    for row in allrows:
-        writer.writerow(row)
-fw.close()
+import os
+
+
+def split(filehandler, delimiter=',', row_limit=1000000,
+          output_name_template='output_%s.csv', output_path='data/train', keep_headers=True):
+    import csv
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    current_piece = 1
+    current_out_path = os.path.join(
+        output_path,
+        output_name_template % current_piece
+    )
+    current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    current_limit = row_limit
+    if keep_headers:
+        headers = reader.next()
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+                output_path,
+                output_name_template % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+
+split(open('data/train.csv', 'r'));
